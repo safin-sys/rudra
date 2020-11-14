5982eb0d8255a1f299e5b85bcb7dce4f69bff716
Safin Ahmed <61120114+safin-sys@users.noreply.github.com>
2020-11-14 15:54:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const ham = document.querySelector('nav > button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+ham.addEventListener('click', e => {
+    ham.classList.toggle('ham-active')
+    mobileNav.classList.toggle('mobile-active')
+})
